46d795410ea07bce5b8a075b16fe2fd23fac87f7
HBence0408 <61012730+HBence0408@users.noreply.github.com>
2025-09-04 16:46:44 +0200

    takarodjon

--- frontend/my-angular-app/src/app/app.ts
+++ frontend/my-angular-app/src/app/app.ts
@@ -1,8 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TicTacToe } from "./tic-tac-toe/tic-tac-toe";
-import { HttpClient } from '@angular/common/http';
-import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -13,20 +11,4 @@ import { ApiService } from './api.service';
 })
 export class App {
   protected readonly title = signal('my-angular-app');
-
-   message: string = '';
-
-  constructor(private api: ApiService) {}
-
-  callBackend() {
-  this.api.getMessage().subscribe(
-    (res: any) => {
-      this.message = res.message;
-    },
-    (err: any) => {
-      this.message = 'Error connecting to backend';
-      console.error(err);
-    }
-  );
-}
 }
